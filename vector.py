a668b78382ea49eb0855cd4b8255125b1b609f21
Velimir-Dimitrov <dark7emplar@gmail.com>
2025-06-10 01:37:00 +0300

    fix slow loading

--- vector.py
+++ vector.py
@@ -5,7 +5,7 @@ import os
 import pandas as pd
 
 # Load the cleaned CSV file
-df = pd.read_csv("children_stories.csv")
+df = pd.read_csv("children_stories.csv").head(10) #load only the first 10 stories
 
 # Set up embeddings and database location
 embeddings = OllamaEmbeddings(model="gemma:2b")
@@ -16,12 +16,12 @@ add_documents = not os.path.exists(db_location)
 if add_documents:
     documents = []
     ids = []
-
+    print('stage 2')
     for i, row in df.iterrows():
         page_content = str(row["Names"]).strip() + " " + str(row["Description"]).strip()
         document = Document(
             page_content=page_content,
-            metadata={"age": str(row["Age"]).strip(), "id": str(i)}
+            metadata={"category": str(row["Category"]).strip(), "id": str(i)}
         )
         documents.append(document)
         ids.append(str(i))
@@ -36,13 +36,12 @@ vector_store = Chroma(
 if add_documents:
     vector_store.add_documents(documents=documents, ids=ids)
 
-# checking if vector base works - retrieve a few stories
-print("Sample embedded stories:")
-sample_docs = vector_store.get(include=["documents"], ids=["0", "1", "2"])
-for i, doc in enumerate(sample_docs["documents"]):
-    print(f"\n--- Story {i + 1} ---\n{doc}")
+# # checking if vector base works - retrieve a few stories
+# sample_docs = vector_store.get(include=["documents"], ids=["0", "1", "2"])
+# for i, doc in enumerate(sample_docs["documents"]):
+#     print(f"\n--- Story {i + 1} ---\n{doc}")
 
 # Set up retriever
 retriever = vector_store.as_retriever(
-    search_kwargs={"k": 5}
+    search_kwargs={"k": 10}
 )
